210d79a558869835d1419cbdd530277713a32fd6
zamaeil <izmaeil.win@gmail.com>
2017-05-07 22:30:13 +0700

    player fix

--- farmapp/src/entity/Player.java
+++ farmapp/src/entity/Player.java
@@ -1,30 +1,27 @@
 package entity;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
-
-import library.SeedLibrary;
-import main.main;
+import main.Input;
 import model.Item;
 
 public class Player {
-	String name;
-	String money;
-	ArrayList<Item> inventory = new ArrayList<Item>();
+	private String name;
+	private double money;
+	private ArrayList<Item> inventory = new ArrayList<Item>();
 
 	public Player() {
 		System.out.println("Welcome. Please enter your name");
-		name = main.scan.next();
-		do {
-			System.out.println("Please enter starting money");
-			money = main.scan.next();
-			if (isNumeric(money) == true) {
-				this.name = name;
-				this.money = money;
-			} else {
-				System.out.println("Please input number");
-			}
-		} while (isNumeric(money) == false);
+		name = Input.getString("","Please enter your name");
+		System.out.println("Please enter starting money");
+		money = Input.getDouble("", "Please input number");
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public double getMoney(){
+		return money;
 	}
 	
 	public ArrayList<Item> getSeeds(){
@@ -75,23 +72,5 @@ public class Player {
 			}
 		}
 		return false;
-	}
-
-	public static boolean isNumeric(String str) {
-		try {
-			double d = Double.parseDouble(str);
-		} catch (NumberFormatException nfe) {
-			return false;
-		}
-		return true;
-	}
-	
-	public static boolean isInteger(String str) {
-		try {
-			int d = Integer.parseInt(str);
-		} catch (NumberFormatException nfe) {
-			return false;
-		}
-		return true;
 	}
 }
